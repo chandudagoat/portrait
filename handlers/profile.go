6e30538d56553e2c88599b0c7226e66cd9a67cba
hclanka <152237163+chandudagoat@users.noreply.github.com>
2023-12-28 16:10:01 +0530

    added unique usernames

--- handlers/profile.go
+++ handlers/profile.go
@@ -3,28 +3,64 @@ package handlers
 import (
 	"context"
 	"fmt"
+	"log"
 	"profiley/database"
 	"profiley/models"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
-func CreateProfile(ctx *fiber.Ctx) error {
+func CreateProfile(c *fiber.Ctx) error {
 	profile := new(models.Profile)
 
-	if err := ctx.BodyParser(&profile); err != nil {
+	if err := c.BodyParser(&profile); err != nil {
 		fmt.Println("error: ", err)
-		return ctx.SendStatus(200)
+		return c.SendStatus(200)
 	}
 
 	collection := database.GetCollection("profiles")
-	result, err := collection.InsertOne(context.TODO(), profile)
 
-	fmt.Println(result)
+	var posts []models.Profile
+	cursor, err := collection.Find(context.TODO(), bson.D{{}})
 
 	if err != nil {
 		panic(err)
 	}
 
-	return ctx.SendString("success")
+	for cursor.Next(context.TODO()) {
+		var elem models.Profile
+		err := cursor.Decode(&elem)
+
+		if err != nil {
+			panic(err)
+		}
+
+		posts = append(posts, elem)
+	}
+
+	if err := cursor.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	cursor.Close(context.TODO())
+
+	for _, post := range posts {
+		if profile.Username == post.Username {
+			return c.SendString("Sorry, this username already exists. Please change it.")
+		} else {
+			_, err := collection.InsertOne(context.TODO(), profile)
+			fmt.Println("created profile")
+
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+	return c.SendString("success")
+}
+
+func GetProfile(c *fiber.Ctx) error {
+	return nil
 }
--- models/profile.go
+++ models/profile.go
@@ -2,6 +2,7 @@ package models
 
 type Profile struct {
 	Name     string  `json: "name" bson: "name"`
+	Username string  `json: "username" bson: "username"`
 	Bio      string  `json: "bio" bson: "bio"`
 	Pronouns string  `json: "pronouns" bson: "pronouns"`
 	Links    Socials `json: "links" bson: "links"`
