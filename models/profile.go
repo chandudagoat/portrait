b7295380fde776191d00f1b4d3f2548873d7b5b6
hclanka <152237163+chandudagoat@users.noreply.github.com>
2024-01-02 23:12:25 +0530

    added email field for the portrait

--- models/profile.go
+++ models/profile.go
@@ -2,6 +2,7 @@ package models
 
 type Profile struct {
 	Name     string  `json: "name" bson: "name"`
+	Email    string  `json: "email" bson: "email"`
 	Username string  `json: "username" bson: "username"`
 	Bio      string  `json: "bio" bson: "bio"`
 	Pronouns string  `json: "pronouns" bson: "pronouns"`
